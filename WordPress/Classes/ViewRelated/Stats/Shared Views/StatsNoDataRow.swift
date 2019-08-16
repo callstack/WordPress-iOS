bb2a69a1ac60a85c59b03aca427f7a2681385e00
Daniele Bogo <daniele.bogo@automattic.com>
2019-08-16 18:25:59 +0100

    Update no data row

--- WordPress/Classes/ViewRelated/Stats/Shared Views/StatsNoDataRow.swift
+++ WordPress/Classes/ViewRelated/Stats/Shared Views/StatsNoDataRow.swift
@@ -9,12 +9,15 @@ class StatsNoDataRow: UIView, NibLoadable, Accessible {
                                                         comment: "Text displayed when an Insights stat section has no data.")
     private let periodNoDataLabel = NSLocalizedString("No data for this period",
                                                       comment: "Text displayed when Period stat section has no data.")
+    private let errorDataLabel = NSLocalizedString("An error occurred.",
+                                                   comment: "Text displayed when stat section got an error.")
 
     // MARK: - Configure
 
-    func configure(forType statType: StatType) {
-        noDataLabel.text = statType == .insights ? insightsNoDataLabel : periodNoDataLabel
+    func configure(forType statType: StatType, rowStatus: StoreFetchingStatus = .idle) {
         WPStyleGuide.Stats.configureLabelAsNoData(noDataLabel)
+        setText(for: statType, rowStatus: rowStatus)
+        animateGhostView(rowStatus == .loading)
         prepareForVoiceOver()
     }
 
@@ -23,5 +26,25 @@ class StatsNoDataRow: UIView, NibLoadable, Accessible {
 
         accessibilityLabel = noDataLabel.text
         accessibilityTraits = .staticText
+    }
+}
+
+private extension StatsNoDataRow {
+    func animateGhostView(_ animate: Bool) {
+        noDataLabel.isGhostableDisabled = false
+        if animate {
+            startGhostAnimation()
+        } else {
+            stopGhostAnimation()
+        }
+    }
+
+    func setText(for statType: StatType, rowStatus: StoreFetchingStatus) {
+        switch rowStatus {
+        case .success, .idle:
+            noDataLabel.text = statType == .insights ? insightsNoDataLabel : periodNoDataLabel
+        default:
+            noDataLabel.text = errorDataLabel
+        }
     }
 }
